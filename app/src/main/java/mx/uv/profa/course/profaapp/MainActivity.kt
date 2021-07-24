2ec7e6a80f4f8652125bca249c0e94064534e0a2
V Josué Aguilera Rueda <josueaguilera@iMac-de-V.local>
2021-07-24 14:08:45 -0500

    ContactsApp ProFA Update

---
+++ app/src/main/java/mx/uv/profa/course/profaapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package mx.uv.profa.course.profaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var name = "Josue"
+        Log.d("Kotlin", name)
+    }
+}
